61d412ea1abc69719d765a264c4ce9612a52117b
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-05-21 13:16:51 -0300

    Renomeados parametros

--- src/sistema_bancario/banco.py
+++ src/sistema_bancario/banco.py
@@ -2,26 +2,26 @@ from typing import Any, Callable
 from . import contas, depositos, exceptions, saques
 
 
-def realizar_deposito(conta: contas.Conta, depo: depositos.Deposito) -> None:
-    contas.adicionar_operacao(conta, depo)
-    contas.adicionar_saldo(conta, depositos.valor_deposito(depo))
+def realizar_deposito(conta: contas.Conta, deposito: depositos.Deposito) -> None:
+    contas.adicionar_operacao(conta, deposito)
+    contas.adicionar_saldo(conta, depositos.valor_deposito(deposito))
 
 
-def realizar_saque(conta: contas.Conta, ssaque: saques.Saque) -> None:
+def realizar_saque(conta: contas.Conta, saque: saques.Saque) -> None:
     if contas.quantidade_saques_do_dia(conta) >= contas.maximo_saques_diarios(conta):
         raise exceptions.QuantidadeDeSaquesSuperiorAoLimiteException(
             "Quantidade de saques realizados superior ao máximo permitido para o dia"
         )
     
-    if contas.valor_maximo_saque(conta) < saques.valor_saque(ssaque):
+    if contas.valor_maximo_saque(conta) < saques.valor_saque(saque):
         raise exceptions.SaqueAcimaDoValorLimiteException(
             "Valor do saque não pode ser superior ao valor limite da conta")
     
-    if contas.saldo_conta(conta) - saques.valor_saque(ssaque) < 0:
+    if contas.saldo_conta(conta) - saques.valor_saque(saque) < 0:
         raise exceptions.SaldoInsuficienteException("Saldo insuficiente para realizar o saque")
 
-    contas.adicionar_operacao(conta, ssaque)
-    contas.adicionar_saldo(conta,  (-1) * saques.valor_saque(ssaque))
+    contas.adicionar_operacao(conta, saque)
+    contas.adicionar_saldo(conta,  (-1) * saques.valor_saque(saque))
     contas.aumentar_quantidade_saques(conta)
 
 
