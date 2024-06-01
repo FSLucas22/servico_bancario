d5123a0b83ff9b0fbcc28560a0afb21002a7bc2a
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-05-31 23:35:49 -0300

    adiciona novos campos em Conta

--- tests/test_conta.py
+++ tests/test_conta.py
@@ -1,10 +1,18 @@
+from datetime import datetime
 import pytest
 import src.sistema_bancario as app
 
 
 @pytest.fixture
 def conta() -> app.contas.Conta:
-    return app.contas.criar_conta()
+    usuario = app.usuarios.criar_usuario(
+        "Lucas",
+        datetime(1999, 6, 22),
+        "123456789",
+        "logradouro, 0001 - bairro - cidade/UF"
+    )
+    numero_conta = 1
+    return app.contas.criar_conta(usuario, numero_conta)
 
 
 def test_deve_criar_conta_sem_operacoes(conta) -> None:
@@ -18,3 +26,17 @@ def test_conta_deve_ter_limite_de_saque_de_500_reais(conta) -> None:
 
 def test_deve_ter_maximo_de_3_saques_diarios(conta) -> None:
     assert app.contas.maximo_saques_diarios(conta) == 3
+
+
+def test_deve_ter_usuario(conta) -> None:
+    usuario = app.contas.usuario_conta(conta)
+    assert usuario is not None
+    assert app.usuarios.cpf_usuario(usuario) == "123456789"
+
+
+def test_deve_ter_numero_da_conta(conta) -> None:
+    assert app.contas.numero_conta(conta) == 1
+
+
+def test_deve_ter_agencia(conta) -> None:
+    assert app.contas.agencia_conta(conta) == "0001"
