729efc65cfd784f44e41fa866d4e7c5d7f186b34
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-05-31 21:14:50 -0300

    refatora construção de conta com fixture

--- tests/test_conta.py
+++ tests/test_conta.py
@@ -1,18 +1,20 @@
+import pytest
 import src.sistema_bancario as app
 
 
-def test_deve_criar_conta_sem_operacoes() -> None:
-    conta = app.contas.criar_conta()
+@pytest.fixture
+def conta() -> app.contas.Conta:
+    return app.contas.criar_conta()
+
+
+def test_deve_criar_conta_sem_operacoes(conta) -> None:
     assert app.contas.quantidade_operacoes(conta) == 0
     assert app.contas.saldo_conta(conta) == 0.0
 
 
-def test_conta_deve_ter_limite_de_saque_de_500_reais() -> None:
-    conta = app.contas.criar_conta()
+def test_conta_deve_ter_limite_de_saque_de_500_reais(conta) -> None:
     assert app.contas.valor_maximo_saque(conta) == 500.0
 
 
-def test_deve_ter_maximo_de_3_saques_diarios() -> None:
-    conta = app.contas.criar_conta()
+def test_deve_ter_maximo_de_3_saques_diarios(conta) -> None:
     assert app.contas.maximo_saques_diarios(conta) == 3
-
