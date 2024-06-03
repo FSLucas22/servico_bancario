c5c78860e7d66a714916615c1ce93f1291d1d7e0
FSLucas22 <lucassantos@presconinformatica.com.br>
2024-06-03 16:32:46 -0300

    adiciona função para deletar parametro da seção

--- src/sistema_bancario/secoes.py
+++ src/sistema_bancario/secoes.py
@@ -8,13 +8,19 @@ def criar_secao() -> Secao:
     return Secao({})
 
 
-def parametros_secao(secao: Secao) -> tuple[str]:
+def parametros_secao(secao: Secao) -> tuple[str, ...]:
     return tuple(secao.keys())
 
 
-def salvar_parametro(secao: Secao, nome: str, valor: Any) -> str:
+def salvar_parametro(secao: Secao, nome: str, valor: Any) -> None:
     secao[nome] = valor
 
 
-def valor_parametro(secao: Secao, nome: str) -> str:
+def valor_parametro(secao: Secao, nome: str) -> Any | None:
     return secao.get(nome)
+
+
+def deletar_parametro(secao: Secao, nome: str) -> None:
+    if nome not in secao:
+        return
+    del secao[nome]
--- tests/test_secao.py
+++ tests/test_secao.py
@@ -1,23 +1,38 @@
+import pytest
 import src.sistema_bancario as app
 
 
-def test_deve_criar_secao_sem_dados() -> None:
-    secao = app.secoes.criar_secao()
+@pytest.fixture
+def secao() -> app.secoes.Secao:
+    return app.secoes.criar_secao()
+
+
+def test_deve_criar_secao_sem_dados(secao) -> None:
     assert app.secoes.parametros_secao(secao) == tuple()
 
 
-def test_deve_salvar_parametro_na_sessao() -> None:
-    secao = app.secoes.criar_secao()
+def test_deve_salvar_parametro_na_sessao(secao) -> None:
     app.secoes.salvar_parametro(secao, "parametro", "abcd")
     assert app.secoes.parametros_secao(secao) == ("parametro",)
 
 
-def test_deve_retornar_valor_salvo_na_sessao() -> None:
-    secao = app.secoes.criar_secao()
+def test_deve_retornar_valor_salvo_na_sessao(secao) -> None:
     app.secoes.salvar_parametro(secao, "parametro", "abcd")
     assert app.secoes.valor_parametro(secao, "parametro") == "abcd"
 
 
-def test_deve_retornar_None_quando_parametro_nao_existe() -> None:
-    secao = app.secoes.criar_secao()
+def test_deve_retornar_None_quando_parametro_nao_existe(secao) -> None:
     assert app.secoes.valor_parametro(secao, "parametro") is None
+
+
+def test_deve_excluir_parametro_da_secao(secao) -> None:
+    app.secoes.salvar_parametro(secao, "parametro", "abcd")
+    app.secoes.deletar_parametro(secao, "parametro")
+    assert app.secoes.parametros_secao(secao) == tuple()
+    assert app.secoes.valor_parametro(secao, "parametro") == None
+
+
+def test_nao_deve_acontecer_nada_quando_parametro_excluido_nao_existe(secao) -> None:
+    app.secoes.deletar_parametro(secao, "parametro")
+    assert app.secoes.parametros_secao(secao) == tuple()
+    assert app.secoes.valor_parametro(secao, "parametro") == None
